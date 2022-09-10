e2d988e373916b97e8317e1a2d36047a3023ac89
suelytohm <suelytohm.02@gmail.com>
2022-09-10 18:10:51 -0300

    navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,15 @@
 import './Navbar.css'
 import logo from '../../assets/images/logo.png'
+import instagram from '../../assets/images/instagram.png'
+import ifood from '../../assets/images/ifood.png'
+import whatsapp from '../../assets/images/whatsapp.png'
 import { Link } from 'react-router-dom'
 import { Button } from '../Button'
 
 export const Navbar = () => {
   return (
     <nav>
+      <div></div>
       <div className="brand">
         <Link to="/">
           <img src={logo} alt="logo" />
@@ -19,10 +23,21 @@ export const Navbar = () => {
         <Link to="/">Contato</Link>
       </div>
       <div className="actions">
-        <Button>Contato</Button>
-        <Button>Contato</Button>
-        <Button>Contato</Button>
+        <Link to="/">
+          <img src={ifood} id="ifood" alt="ifood" />
+        </Link>
+        <Link to="/">
+          <img src={instagram} id="instagram" alt="instagram" />
+        </Link>
+        <div className="vertical-separator"></div>
+        <Link to="/">
+          <Button>
+            <img src={whatsapp} alt="whatsapp" />
+            Contato
+          </Button>
+        </Link>
       </div>
+      <div></div>
     </nav>
   )
 }
